2da7f7e789b1ff09aea67831859e599c1e0c7c9d
Nathan Gill <nrgill28@gmail.com>
2021-02-05 17:29:55 -0500

    Fixed a bunch of setting stuff and set build target to .NET 5

--- Slicer/Settings.cs
+++ Slicer/Settings.cs
@@ -1,28 +1,22 @@
-﻿namespace Slicer.Properties {
-    
-    
-    // This class allows you to handle specific events on the settings class:
-    //  The SettingChanging event is raised before a setting's value is changed.
-    //  The PropertyChanged event is raised after a setting's value is changed.
-    //  The SettingsLoaded event is raised after the setting values are loaded.
-    //  The SettingsSaving event is raised before the setting values are saved.
+﻿using ModernWpf;
+
+namespace Slicer.Properties {
     internal sealed partial class Settings {
         
-        public Settings() {
-            // // To add event handlers for saving and changing settings, uncomment the lines below:
-            //
-            // this.SettingChanging += this.SettingChangingEventHandler;
-            //
-            // this.SettingsSaving += this.SettingsSavingEventHandler;
-            //
+        public Settings()
+        {
+            PropertyChanged += Settings_PropertyChanged;
+            SettingsLoaded += Settings_SettingsLoaded;
         }
-        
-        private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e) {
-            // Add code to handle the SettingChangingEvent event here.
+
+        private void Settings_SettingsLoaded(object sender, System.Configuration.SettingsLoadedEventArgs e)
+        {
+            ThemeManager.Current.ApplicationTheme = EnableDarkMode ? ApplicationTheme.Dark : ApplicationTheme.Light;
         }
-        
-        private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e) {
-            // Add code to handle the SettingsSaving event here.
+
+        private void Settings_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            Save();
         }
     }
 }
