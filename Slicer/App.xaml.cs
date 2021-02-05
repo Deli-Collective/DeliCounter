5eb0cee3b50ea888b802da5d59be0b995d523f8c
Nathan Gill <nrgill28@gmail.com>
2021-02-05 17:18:13 -0500

    Okay nevermind, figured out the issue.

--- Slicer/App.xaml.cs
+++ Slicer/App.xaml.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
-namespace Slicer
+namespace WpfApp1
 {
-    public partial class App
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
     {
-
     }
 }
